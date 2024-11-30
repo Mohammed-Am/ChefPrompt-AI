ffc2460e93a7276b8c5553560600838009fc88ec
Mohammed-Am <mh.amouzoun@gmail.com>
2024-11-30 18:42:48 +0100

    add form

---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+import '../index.css'
+
+export default function Main(){
+    return (
+    <main>
+        <form>
+            <input 
+                type="text"
+                aria-label=''
+                placeholder='e.g. oregano'
+            
+            />
+            <button>Add ingredient</button>
+        </form>
+    </main>    
+    )
+}
