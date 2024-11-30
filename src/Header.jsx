7dbd2093d4fcf3f00548f3eb2b6a1ad4d89cce64
Mohammed-Am <mh.amouzoun@gmail.com>
2024-11-30 17:23:36 +0100

    commit message

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+
+import './index.css'
+export default function Header(){
+    return(
+        <>
+            <header>
+              <img src ='/images/ChefClaudeLogo.png'/>
+              <p>Chef Prompt AI</p>
+            </header>
+        </>
+    )
+}
