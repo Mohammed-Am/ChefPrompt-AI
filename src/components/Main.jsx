1ade4147dceb1a236a060e892e96997ed38f56b5
Mohammed-Am <mh.amouzoun@gmail.com>
2024-12-11 17:31:55 +0100

    Refactor array state

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,43 @@
 import '../index.css'
+import React from 'react'
 
 export default function Main(){
+
+    const [ingredients , setIngredients] = React.useState([])
+
+    const ingredientListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+
+    function addIngredient(event){
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget)
+        const newIngredients = formData.get("ingredient");
+        
+        setIngredients(prev => [...prev , newIngredients])
+    }
+
     return (
     <main>
-        <form>
+        <form onSubmit={addIngredient}>
             <input 
                 type="text"
                 aria-label=''
                 placeholder='e.g. oregano'
+                name='ingredient'
             
             />
-            <button>Add ingredient</button>
+            <button 
+                aria-label='Add ingredient'
+            >
+            Add ingredient
+            </button>
         </form>
+
+        <ul>
+            {ingredientListItems}
+        </ul>
     </main>    
     )
 }
